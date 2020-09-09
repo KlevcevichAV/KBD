f8e0ad9b974074624b1c2e5de20c30d28c63cc68
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-09-09 15:23:00 +0300

    style(lab1): translated into English task_2

--- KBD_1/task_2.sql
+++ KBD_1/task_2.sql
@@ -1,7 +1,7 @@
 drop table provider;
 drop table part;
 drop table project;
-drop table producer_part_project;
+drop table provider_part_project;
 
 create table provider
 (
@@ -9,29 +9,33 @@ create table provider
     ProviderName   VARCHAR(10),
     Status int,
     City  VARCHAR(10)
-);
-create table item
+)CHARACTER SET utf8
+ COLLATE utf8_unicode_ci;;
+create table part
 (
-    ItemID      VARCHAR(2) primary key,
-    ItemName   VARCHAR(10),
+    partID      VARCHAR(2) primary key,
+    partName   VARCHAR(10),
     Color   VARCHAR(10),
     Size INT,
     City  VARCHAR(10)
-);
+)CHARACTER SET utf8
+ COLLATE utf8_unicode_ci;;
 create table project
 (
     ProjectID    VARCHAR(3) primary key,
     ProjectName VARCHAR(4),
     City VARCHAR(10)
-);
+)CHARACTER SET utf8
+ COLLATE utf8_unicode_ci;;
 create table provider_part_project
 (
     ProviderID  VARCHAR(2),
-    ItemID  VARCHAR(2),
+    partID  VARCHAR(2),
     ProjectID VARCHAR(3),
     S  int,
-    primary key (ProviderID, ItemID, ProjectID)
-);
+    primary key (ProviderID, partID, ProjectID)
+)CHARACTER SET utf8
+ COLLATE utf8_unicode_ci;;
 
 insert into provider (ProviderID, ProviderName, Status, City)
 values ('П1', 'Петров', 20, 'Москва'),
@@ -39,7 +43,7 @@ values ('П1', 'Петров', 20, 'Москва'),
        ('П3', 'Федоров', 30, 'Таллинн'),
        ('П4', 'Чаянов', 20, 'Минск'),
        ('П5', 'Крюков', 30, 'Киев');
-insert into item (ItemID, ItemName, Color, Size, City)
+insert into part (partID, partName, Color, Size, City)
 values ('Д1', 'Болт', 'Красный', 12, 'Москва'),
        ('Д2', 'Гайка', 'Зелёная', 17, 'Минск'),
        ('Д3', 'Диск', 'Черный', 17, 'Вильнюс'),
@@ -54,7 +58,7 @@ values ('ПР1', 'ИПР1', 'Минск'),
        ('ПР5', 'ИПР5', 'Москва'),
        ('ПР6', 'ИПР6', 'Саратов'),
        ('ПР7', 'ИПР7', 'Москва');
-insert into provider_part_project (ProjectID, ItemID, ProjectID, S)
+insert into provider_part_project (ProviderID, partID, ProjectID, S)
 values ('П1', 'Д1', 'ПР1', 200),
        ('П1', 'Д1', 'ПР2', 700),
        ('П2', 'Д3', 'ПР1', 400),
@@ -82,21 +86,21 @@ values ('П1', 'Д1', 'ПР1', 200),
 
 # 20 Получить цвета деталей, поставляемых поставщиком П1.
 select distinct Color
-from item p
-         left join provider_part_project ppp on p.ItemID = ppp.ItemID
-where ppp.ItemID = 'П1';
+from part p
+         left join provider_part_project ppp on p.partID = ppp.partID
+where ppp.partID = 'П1';
 # 23 Получить номера поставщиков, поставляющих по крайней мере одну деталь, поставляемую по крайней мере одним поставщиком, который поставляет по крайней мере одну красную деталь.
 
 # 31 Получить номера поставщиков, поставляющих одну и ту же деталь для всех проектов.
 select t2.producer, count(*)
 from (
-         select t.producer producer, t.part part
+         select t.provider producer, t.part part
          from (
-                  select ПР project, П producer, Д part
-                  from producer_part_project ppp
-                  group by ПР, П, Д
+                  select ProjectID project, ProviderID provider, partID part
+                  from provider_part_project ppp
+                  group by project, provider, part
               ) t
-         group by t.producer, t.part
+         group by t.provider, t.part, t.part
      ) t2
 group by t2.producer
 having count(*) = 6;
@@ -105,37 +109,37 @@ select *
 from project
 where City = 'Лондон';
 # 9 Получить номера деталей, поставляемых поставщиком в Лондоне.
-select Д
+select partID
 from part
-where Город = 'Лондон';
+where City = 'Лондон';
 # 13 Получить номера проектов, обеспечиваемых по крайней мере одним поставщиком не из того же города.
 select t.project
-from (select ppp.ПР project, p.City sity, count(*) cnt
+from (select ppp.ProviderID project, p.City sity, count(*) cnt
       from provider p
-               left join producer_part_project ppp on p.ProviderID = ppp.П
-      group by ppp.ПР, p.City) t
+               left join provider_part_project ppp on p.ProviderID = ppp.ProviderID
+      group by ppp.ProjectID, p.City) t
 group by project
 having count(*) > 1;
 # 17 Для каждой детали, поставляемой для проекта, получить номер детали, номер проекта и соответствующее общее количество. ??? XD
-select Д, ПР, S from producer_part_project;
+select partID, ProjectID, S from provider_part_project;
 # 36 Получить все пары номеров поставщиков, скажем, Пx и Пy, такие, что оба эти поставщика поставляют в точности одно и то же множество деталей.
-select t.П, t2.П
+select t.ProviderID, t2.ProviderID
 from (
-         select П, SUM(t.S) S
-         from producer_part_project t
-         group by П
+         select ProviderID, SUM(t.S) S
+         from provider_part_project t
+         group by ProviderID
      ) t,
      (
-         select П, SUM(t.S) S
-         from producer_part_project t
-         group by П
+         select ProviderID, SUM(t.S) S
+         from provider_part_project t
+         group by ProviderID
      ) t2
 where t2.S = t.S
-  and t.П <> t2.П;
+  and t.ProviderID <> t2.ProviderID;
 # 27 Получить номера поставщиков, поставляющих деталь Д1 для некоторого проекта в количестве, большем среднего количества деталей Д1 в поставках для этого проекта.
 
 # 18 Получить номера деталей, поставляемых для некоторого проекта со средним количеством больше 320. ????
-select Д
-from producer_part_project
-group by Д
+select partID
+from provider_part_project
+group by partID
 having avg(S) > 320;
