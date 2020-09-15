61dad2ade5f84fda1d94b4e5468a2500cd41ad1b
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-09-15 16:48:45 +0300

    feat(lab1): added all requests

--- KBD_1/scripts/task_1/queries/querie_19.sql
+++ KBD_1/scripts/task_1/queries/querie_19.sql
@@ -1,1 +1,6 @@
 #19. Получить номера групп с такой же специальностью, что и специальность преподавателей.
+SELECT DISTINCT sg.student_group_code
+FROM teacher_student_group AS tsg
+         LEFT JOIN student_group AS sg on tsg.student_group_code = sg.student_group_code
+         LEFT JOIN teacher AS t on tsg.personal_number = t.personal_number
+WHERE sg.specialty = t.specialty;
--- KBD_1/scripts/task_1/queries/querie_20.sql
+++ KBD_1/scripts/task_1/queries/querie_20.sql
@@ -1,1 +1,6 @@
 #20. Получить номера преподавателей с кафедры ЭВМ, преподающих предметы по специальности, совпадающей со специальностью студенческой группы.
+SELECT DISTINCT personal_number
+FROM teacher_student_group AS tsg
+         LEFT JOIN student_group AS sg on tsg.student_group_code = sg.student_group_code
+         left join subject AS s on tsg.subject_code_number = s.subject_code_number
+WHERE sg.specialty = s.specialty;
