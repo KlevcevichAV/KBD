7a6d7a7dc7d37555735647f7ccbe37db648357b4
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-09-20 12:20:23 +0300

    feat(lab2): created tables

---
+++ KBD_2/src/sample/database/location_of_equipment.sql
@@ -0,0 +1,7 @@
+CREATE TABLE location_of_equipment
+(
+    transmission_date DATE,
+    full_name         VARCHAR(30),
+    position          VARCHAR(10),
+    room_number       INT
+);
---
+++ KBD_2/src/sample/database/subdivision.sql
@@ -0,0 +1,6 @@
+CREATE TABLE location_of_equipment
+(
+    number     INT PRIMARY KEY,
+    full_name  VARCHAR(30),
+    short_name VARCHAR(10)
+);
