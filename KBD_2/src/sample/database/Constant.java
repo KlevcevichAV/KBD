7478293c1d8a45991ea83b4f76baa609bf5c0f59
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-09-23 00:25:35 +0300

    feat(lab2): added delete operation

--- KBD_2/src/sample/database/Constant.java
+++ KBD_2/src/sample/database/Constant.java
@@ -10,12 +10,28 @@ public class Constant {
 
     public static String INSERT = "INSERT INTO ";
 
-    public static String VALUES_EQUIPMENT = "(inventory_number, name, date_of_purchase, price)";
-    public static String VALUES_LOCATION_OF_EQUIPMENT = "(transmission_date, full_name, position, room_number)";
-    public static String VALUES_SUBDIVISION = "(number, full_name, short_name)";
+    public static String DELETE = "DELETE FROM ";
+
+    public static String WHERE = " WHERE ";
+    public static String AND = " AND ";
+
     public static String VALUES = "VALUES ";
+    public static String VALUES_EQUIPMENT = "(inventory_number, name, date_of_purchase, price)";
+    public static String INVENTORY_NUMBER = "inventory_number";
+    public static String NAME = "name";
+    public static String DATE_OF_PURCHASE = "date_of_purchase";
+    public static String PRICE = "price";
+    public static String VALUES_LOCATION_OF_EQUIPMENT = "(transmission_date, full_name, position, room_number)";
+    public static String TRANSMISSION_DATE = "transmission_date";
+    public static String FULL_NAME = "full_name";
+    public static String POSITION = "position";
+    public static String ROOM_NUMBER = "room_number";
+    public static String VALUES_SUBDIVISION = "(number, full_name, short_name)";
+    public static String NUMBER = "number";
+    public static String SHORT_NAME = "short_name";
 
     public static String COMMA = ", ";
+    public static String EQUAL = " = ";
     public static String SEMICOLON = ";";
     public static String LEFT_BRACKET = "(";
     public static String RIGHT_BRACKET = ")";
--- KBD_2/src/sample/database/DataBase.java
+++ KBD_2/src/sample/database/DataBase.java
@@ -1,5 +1,6 @@
 package sample.database;
 //add apostrophes for string getters
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,7 @@ public class DataBase {
     private List<LocationOfEquipment> locationOfEquipments;
     private List<Subdivision> subdivisions;
 
-    private void settingProperties(){
+    private void settingProperties() {
         p = new Properties();
         p.setProperty("user", "root");
         p.setProperty("password", "123");
@@ -27,8 +28,8 @@ public class DataBase {
         Connection connection = DriverManager.getConnection(url, p);
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(Constant.SELECT_EQUIPMENT);
-            while (resultSet.next()){
-                equipment.add(new Equipment(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), Integer.parseInt(resultSet.getString(3)), Integer.parseInt(resultSet.getString(4)),Integer.parseInt(resultSet.getString(5)), Integer.parseInt(resultSet.getString(6))));
+            while (resultSet.next()) {
+                equipment.add(new Equipment(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), Integer.parseInt(resultSet.getString(3)), Integer.parseInt(resultSet.getString(4)), Integer.parseInt(resultSet.getString(5)), Integer.parseInt(resultSet.getString(6))));
             }
             System.out.println("We're created equipment.");
         }
@@ -41,8 +42,8 @@ public class DataBase {
         Connection connection = DriverManager.getConnection(url, p);
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(Constant.SELECT_EQUIPMENT);
-            while (resultSet.next()){
-                locationOfEquipments.add(new LocationOfEquipment(Integer.parseInt(resultSet.getString(1)), Integer.parseInt(resultSet.getString(2)), Integer.parseInt(resultSet.getString(3)),resultSet.getString(4), resultSet.getString(5), Integer.parseInt(resultSet.getString(6))));
+            while (resultSet.next()) {
+                locationOfEquipments.add(new LocationOfEquipment(Integer.parseInt(resultSet.getString(1)), Integer.parseInt(resultSet.getString(2)), Integer.parseInt(resultSet.getString(3)), resultSet.getString(4), resultSet.getString(5), Integer.parseInt(resultSet.getString(6))));
             }
             System.out.println("We're created location Of Equipments.");
         }
@@ -55,7 +56,7 @@ public class DataBase {
         Connection connection = DriverManager.getConnection(url, p);
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(Constant.SELECT_EQUIPMENT);
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 subdivisions.add(new Subdivision(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), resultSet.getString(3)));
             }
             System.out.println("We're created subdivisions.");
@@ -100,6 +101,41 @@ public class DataBase {
                     added.getShortName() + Constant.RIGHT_BRACKET + Constant.SEMICOLON);
             subdivisions.add(added);
             System.out.println("We're added.");
+        }
+    }
+
+    public void deleteEquipment(Equipment deleted) throws SQLException {
+        Connection connection = DriverManager.getConnection(url, p);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(Constant.DELETE + Constant.EQUIPMENT + Constant.WHERE +
+                    Constant.INVENTORY_NUMBER + Constant.EQUAL + deleted.getInventoryNumber() + Constant.AND +
+                    Constant.NAME + Constant.EQUAL + deleted.getName() + Constant.AND +
+                    Constant.DATE_OF_PURCHASE + Constant.EQUAL + Constant.intToDate(deleted.getDay(), deleted.getMonth(), deleted.getYear()) + Constant.AND +
+                    Constant.PRICE + Constant.EQUAL + deleted.getPrice() + Constant.SEMICOLON
+            );
+        }
+    }
+
+    public void deleteLocationOfEquipments(LocationOfEquipment deleted) throws SQLException {
+        Connection connection = DriverManager.getConnection(url, p);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(Constant.DELETE + Constant.LOCATION_OF_EQUIPMENT + Constant.WHERE +
+                    Constant.TRANSMISSION_DATE + Constant.EQUAL + Constant.intToDate(deleted.getDay(), deleted.getMonth(), deleted.getYear()) + Constant.AND +
+                    Constant.FULL_NAME + Constant.EQUAL + deleted.getFullName() + Constant.AND +
+                    Constant.POSITION + Constant.EQUAL + deleted.getPosition() + Constant.AND +
+                    Constant.ROOM_NUMBER + Constant.EQUAL + deleted.getRoomNumber() + Constant.SEMICOLON
+            );
+        }
+    }
+
+    public void deleteSubdivisions(Subdivision deleted) throws SQLException {
+        Connection connection = DriverManager.getConnection(url, p);
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(Constant.DELETE + Constant.SUBDIVISION + Constant.WHERE +
+                    Constant.NUMBER + Constant.EQUAL + deleted.getNumber() + Constant.AND +
+                    Constant.FULL_NAME + Constant.EQUAL + deleted.getFullName() + Constant.AND +
+                    Constant.SHORT_NAME + Constant.EQUAL + deleted.getShortName() + Constant.SEMICOLON
+            );
         }
     }
 
